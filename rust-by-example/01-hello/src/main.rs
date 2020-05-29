af58775548b8b26f44117ed1bdb371f90f0c6527
tetsuyainfra <tetsuyainfra@gmail.com>
2020-05-29 21:51:32 +0900

    update

--- rust-by-example/01-hello/src/main.rs
+++ rust-by-example/01-hello/src/main.rs
@@ -1,3 +1,21 @@
+use std::fmt;
+
+// #[warn(dead_code)]
+#[allow(dead_code)]
+struct StructureNo(i32);
+
+struct Structure(i32);
+
+impl fmt::Display for Structure {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "impl fmt->{}", self.0)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // let x_no = StructureNo(12);
+    // println!("Hello, world! {}", x_no);
+
+    let x = Structure(12);
+    println!("x: {}", x); // x: impl fmt->12
 }
