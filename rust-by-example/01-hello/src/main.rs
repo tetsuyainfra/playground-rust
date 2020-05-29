d5d1ed5da48dc92b2d95e303e2a0b416e8a9d3ad
tetsuyainfra <tetsuyainfra@gmail.com>
2020-05-29 22:07:03 +0900

    update

--- rust-by-example/01-hello/src/main.rs
+++ rust-by-example/01-hello/src/main.rs
@@ -1,11 +1,11 @@
 use std::fmt;
 
-// #[warn(dead_code)]
+// #[warn(dead_code)] // こっちがデフォルトで未使用エラーを出す
 #[allow(dead_code)]
 struct StructureNo(i32);
 
+// ジェネリックでない型は自由にfmt::Displayを定義してしまえば良い
 struct Structure(i32);
-
 impl fmt::Display for Structure {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "impl fmt->{}", self.0)
@@ -14,7 +14,7 @@ impl fmt::Display for Structure {
 
 fn main() {
     // let x_no = StructureNo(12);
-    // println!("Hello, world! {}", x_no);
+    // println!("Hello, world! {}", x_no); // fmt::Displayトレイトを実装していないのでエラー
 
     let x = Structure(12);
     println!("x: {}", x); // x: impl fmt->12
