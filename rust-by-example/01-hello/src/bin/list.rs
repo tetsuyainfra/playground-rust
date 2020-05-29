053cad976f6e2f6ba12248d73cb4c00abb4b4643
tetsuyainfra <tetsuyainfra@gmail.com>
2020-05-29 22:56:52 +0900

    update

---
+++ rust-by-example/01-hello/src/bin/list.rs
@@ -0,0 +1,26 @@
+use std::fmt;
+
+#[derive(Debug)]
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    let vec = &self.0;
+    write!(f, "[")?;
+
+    for (count, v) in vec.iter().enumerate() {
+      if count != 0 {
+        write!(f, ", ")?;
+      }
+      write!(f, "{}", v)?;
+    }
+
+    write!(f, "]")
+  }
+}
+
+fn main() {
+  let list = List(vec![1, 2, 3, 4, 5]);
+  println!("{}", list);
+  println!("{:?}", list);
+}
