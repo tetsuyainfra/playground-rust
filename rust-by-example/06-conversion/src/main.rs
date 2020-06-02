06ef09c5ebe7a35a0a61548ebda6f637f5829780
tetsuyainfra <tetsuyainfra@gmail.com>
2020-06-02 23:27:02 +0900

    update

--- rust-by-example/06-conversion/src/main.rs
+++ rust-by-example/06-conversion/src/main.rs
@@ -1,3 +1,26 @@
+use std::convert::From;
+
 fn main() {
-    println!("Hello, world!");
+    // 定義されてる
+    let my_str = "hello";
+    let my_string = String::from(my_str);
+
+    // 定義してみる
+    #[derive(Debug)]
+    struct Number {
+        value: i32,
+    }
+
+    impl From<i32> for Number {
+        fn from(item: i32) -> Self {
+            Number { value: item }
+        }
+    }
+
+    let num = Number::from(30);
+    println!("My Number is {:?}", num);
+
+    // Type declaration が必要
+    let num: Number = 5.into();
+    println!("My Number is {:?}", num);
 }
