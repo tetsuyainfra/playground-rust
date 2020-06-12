26afe7404e0b03b71eb26f07e7998d1b31f918a9
tetsuyainfra <tetsuyainfra@gmail.com>
2020-06-12 10:38:19 +0900

    update

--- rust-by-example/18-error_handling/src/bin/18_5_0_iterate.rs
+++ rust-by-example/18-error_handling/src/bin/18_5_0_iterate.rs
@@ -1,3 +1,8 @@
+// filter_map() : 関数を呼んでフィルタを適用した結果がNoneなら取り除く
+// collect()    : イテレーションを途中で終了して、Resutl<Vec<T>, E>にして返す
+//               （ので、Errは一つだし、イテレーションが途中で終わる）
+// partition()  : 実行結果を成功と失敗に分けて返す
+
 // Iterating over Results
 // An Iter::map operation might fail, for example:
 fn main1() {
@@ -11,7 +16,7 @@ fn main1() {
 // filter_map calls a function and filters out the results that are None.
 
 fn main2() {
-  let strings = vec!["tofu", "93", "18"];
+  let strings = vec!["tofu", "93", "18"]; // [93, 18]
   let numbers: Vec<_> = strings
     .into_iter()
     .map(|s| s.parse::<i32>())
@@ -23,15 +28,20 @@ fn main2() {
 // Fail the entire operation with collect()
 // Result implements FromIter so that a vector of results (Vec<Result<T, E>>) can be turned into a result with a vector (Result<Vec<T>, E>). Once an Result::Err is found, the iteration will terminate.
 fn main3() {
-  let strings = vec!["tofu", "93", "18"];
+  let strings = vec!["tofu", "93", "18"]; // -> Result::Errが帰ってくる
   let numbers: Result<Vec<_>, _> = strings.into_iter().map(|s| s.parse::<i32>()).collect();
   println!("Results3: {:?}", numbers);
 }
 
-// Collect all valid values and failures with partition()
+fn main3_2() {
+  let strings = vec!["93", "tofu", "93", "18"]; // -> Result::Errが帰ってくる
+  let numbers: Result<Vec<_>, _> = strings.into_iter().map(|s| s.parse::<i32>()).collect();
+  println!("Results3_2: {:?}", numbers);
+}
 
+// Collect all valid values and failures with partition()
 fn main4() {
-  let strings = vec!["tofu", "93", "18"];
+  let strings = vec!["tofu", "93", "18"]; // (Err, Vec<i32>)になる
   let (numbers, errors): (Vec<_>, Vec<_>) = strings
     .into_iter()
     .map(|s| s.parse::<i32>())
@@ -60,6 +70,7 @@ fn main() {
   main1();
   main2();
   main3();
+  main3_2(); // add
   main4();
   main4_2();
 }
